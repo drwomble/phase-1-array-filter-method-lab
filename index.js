1ea0ce751056bce3ac70f4ea15be223f3a96726a
Drew <drwomble27@gmail.com>
2023-04-16 17:47:05 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+/* function findMatching(name, query){
+return name.filter(name => driver.toLowerCase() === query.toLowerCase());
+} */
+function findMatching(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+  }
+
+function fuzzyMatch(driver, query){
+    return driver.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+}
+function matchName(drivers, query){
+    return drivers.filter(driver => driver.name.toLowerCase() === query.toLowerCase());
+}
